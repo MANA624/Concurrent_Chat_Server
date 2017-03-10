e58e313061af026f6d9bdb45cf8bc509e897f702
Matthew Niemiec <Matthew Niemiec>
2017-03-10 15:23:54 -0700

    Basic code for client/server

---
+++ ChatApp/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String argv[]) throws Exception
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Are you the server? 'y' or 'n': ");
+        char response = input.next().charAt(0);
+        if(response == 'y'){
+            Server server = new Server();
+        }
+        else if (response == 'n'){
+            Client client = new Client();
+        }
+        else {
+            System.out.println("Not a valid response");
+        }
+
+    }
+}
